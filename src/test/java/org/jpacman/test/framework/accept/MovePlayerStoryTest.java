296ec25fa7ec9404d3c1537e8a251bdc3fdda6b1
Arie van Deursen <arie.vandeursen@tudelft.nl>
2013-02-07 19:39:26 +0100

    Added a test class.

---
+++ src/test/java/org/jpacman/test/framework/accept/MovePlayerStoryTest.java
@@ -0,0 +1,10 @@
+package org.jpacman.test.framework.accept;
+
+/**
+ * TODO: Create the acceptance test suites for the moving player.
+ */
+public class MovePlayerStoryTest extends AbstractAcceptanceTest {
+	
+}
+	
+
