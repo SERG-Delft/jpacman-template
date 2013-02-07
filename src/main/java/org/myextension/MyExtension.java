faeec8ec7ca7354fafaeed9308e9ac257597bc54
Arie van Deursen <arie.vandeursen@tudelft.nl>
2013-02-07 19:58:54 +0100

    Added javadocs.

--- src/main/java/org/myextension/MyExtension.java
+++ src/main/java/org/myextension/MyExtension.java
@@ -3,10 +3,21 @@ package org.myextension;
 import org.jpacman.framework.factory.FactoryException;
 import org.jpacman.framework.ui.MainUI;
 
+/**
+ * Extend the JPacman framework by subclassing relevant classes.
+ * Creation of the subclass instances requires adjusting the factory,
+ * and injecting the relevant instances at the right places.
+ */
+
 public class MyExtension extends MainUI {
 	
 	private static final long serialVersionUID = -2368505776408908204L;
 
+	/**
+	 * Start the pacman user interface.
+	 * @param argv Ignored
+	 * @throws FactoryException If board can't be read.
+	 */
 	public static void main(String[] argv) throws FactoryException {
 		(new MyExtension()).main();
 	}
