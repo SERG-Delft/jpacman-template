58f8a9ac152a149268a2ddc349d6e2f0d1c8d9f0
LiamClark <L.A.Clark@student.tudelft.nl>
2018-04-30 21:50:57 +0200

    update jpacman-template to jpacman-8.1.0-SNAPSHOT

--- src/test/java/nl/tudelft/jpacman/npc/ghost/GhostMapParser.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/GhostMapParser.java
@@ -1,23 +1,20 @@
 package nl.tudelft.jpacman.npc.ghost;
 
-import nl.tudelft.jpacman.board.Board;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.board.Square;
-import nl.tudelft.jpacman.board.Unit;
-import nl.tudelft.jpacman.level.Level;
 import nl.tudelft.jpacman.level.LevelFactory;
 import nl.tudelft.jpacman.level.MapParser;
 import nl.tudelft.jpacman.npc.Ghost;
-import nl.tudelft.jpacman.sprite.PacManSprites;
 
 import java.util.List;
 
 /**
  * A test helper utility for writing the ghost unit tests.
+ * A useful method for retrieving ghosts from your map would be:
+ * findUnitInBoard in the Navigation class.
  */
 public final class GhostMapParser extends MapParser {
     private final GhostFactory ghostFactory;
-    private final BoardFactory boardFactory;
 
     /**
      * Creates a new enhanced map parser.
@@ -31,24 +28,7 @@ public final class GhostMapParser extends MapParser {
     private GhostMapParser(LevelFactory levelFactory, BoardFactory boardFactory,
                            GhostFactory ghostFactory) {
         super(levelFactory, boardFactory);
-        this.boardFactory = boardFactory;
         this.ghostFactory = ghostFactory;
-    }
-
-    /**
-     * Utility method for convenient ghostMapParser creation.
-     *
-     * @return A wired up ghostMapParser ready for action!
-     */
-    public static GhostMapParser create() {
-        PacManSprites pacManSprites = new PacManSprites();
-        GhostFactory ghostFactory = new GhostFactory(pacManSprites);
-        return new GhostMapParser(
-                new LevelFactory(pacManSprites, ghostFactory),
-                new BoardFactory(pacManSprites),
-                ghostFactory
-        );
-
     }
 
     //This method only supports clyde for now
@@ -63,36 +43,5 @@ public final class GhostMapParser extends MapParser {
             default:
                 super.addSquare(grid, ghosts, startPositions, x, y, c);
         }
-    }
-
-    /**
-     *  Finds a subtype of Unit in a level.
-     *  This method is very useful for finding the ghosts in the parsed map.
-     *  
-     * @param clazz the type to search for.
-     * @param level the level to search in.
-     * @param <T> the return type, same as the type in clazz.
-     *           
-     * @return the first unit found of type clazz, or null.
-     */
-    public <T extends Unit> T findUnit(Class<T> clazz, Level level) {
-        final Board board = level.getBoard();
-        for (int y = 0; y < board.getHeight(); y++) {
-            for (int x = 0; x < board.getWidth(); x++) {
-                final T ghost = Navigation.findUnit(clazz, board.squareAt(x, y));
-                if (ghost != null) {
-                    return ghost;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private Square makeGhostSquare(List<Ghost> ghosts, Ghost ghost) {
-        Square ghostSquare = this.boardFactory.createGround();
-        ghosts.add(ghost);
-        ghost.occupy(ghostSquare);
-        return ghostSquare;
     }
 }
